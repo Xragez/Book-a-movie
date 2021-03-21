9e5d16539b961ee470eb9977353447e9ae647f0f
pawel <pawelzeg@gmail.com>
2021-03-21 17:12:21 +0100

    Add Movie and Movies components

---
+++ frontend/src/components/Movies/Movies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Movies.module.css';
+
+function Movies() {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>Now Playing</h2>
+            
+        </div>
+    )
+}
---
+++ frontend/src/components/Movies/movie/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Movie.module.css';
+
+function Movie(){
+    return (
+        <div className={`card ${styles.account}`}>
+        </div>
+    );
+}
+
+export default Account;
