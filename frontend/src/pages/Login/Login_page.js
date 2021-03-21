41fa65f800459783afe999da3ce02d780c6c7885
pawel <pawelzeg@gmail.com>
2021-03-21 16:52:29 +0100

    Add login page

--- frontend/src/pages/Login/Login_page.js
+++ frontend/src/pages/Login/Login_page.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import style from './Login_page.module.css';
+import styles from './Login_page.module.css';
 
 export default function Login() {
     return (
-        <div className={style.backgroundContainer}>
+        <div className={styles.backgroundContainer}>
             <div className="container d-flex flex-row p-4">
-                <div className={`${style.pictureContainer} container`}>
+                <div className={`${styles.pictureContainer} container`}>
                 </div>
-                <div className={`${style.formContainer} container`}>
-                    <div className={style.logo}>
+                <div className={`${styles.formContainer} container`}>
+                    <div className={styles.logo}>
                     </div>
-                    <div className={style.formLogin}>
+                    <div className={styles.formLogin}>
                         <h2>Sign in!</h2>
                         <div className="loginForm">
                             <input className="form-control"></input>
                             <input className="form-control"></input>
                             <button className="btn btn-primary pl-5 pr-5">Login</button>
                         </div>
-                        <div className={`${style.loginFooter} d-flex flex-row`}>
+                        <div className={`${styles.loginFooter} d-flex flex-row`}>
                             <h3 className="p-2">Not a member?</h3>
                             <a className="p-2">Sing up.</a>
                         </div>
@@ -26,5 +26,5 @@ export default function Login() {
                 </div>
             </div>
         </div>
-        ) 
+        )
 }
