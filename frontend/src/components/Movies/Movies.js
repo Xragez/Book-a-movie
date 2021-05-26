516b1f21f6447cfe71b0905b48e2935483ce5b02
pawel <pawelzeg@gmail.com>
2021-05-26 16:11:13 +0200

    Add movie database api support

--- frontend/src/components/Movies/Movies.js
+++ frontend/src/components/Movies/Movies.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Movies.module.css';
 import Movie from './Movie/Movie';
+import axios from "axios";
+import Carousel, { slidesToShowPlugin  } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
-function Movies() {
+const API_KEY = process.env.REACT_APP_API_KEY
+
+function Movies(props) {
+
+    const [movies, setMovies] = useState([])
+
+    const optionsMovies = {
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${props.sortBy}&include_adult=false&include_video=false&vote_count.gte=100`
+      };
+    
+    const fetchMovies = () => {   
+        axios.request(optionsMovies).then(function (response) {
+            setMovies(response.data.results);
+            console.log(response.data.results);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+    
+    useEffect(() => {
+        fetchMovies()
+      }, [])
+
+
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5
+      };
+
     return (
-        <div className={`${styles.container} ml-4 mt-4`}>
-            <h3 className={styles.title}>Now Playing:</h3>
+        <div className="mt-3 ml-3">
+            <h3 className={styles.title}>{props.title}</h3>
             <div className="row">
-                <Movie/>
-                <Movie/>
-                <Movie/>
-                <Movie/>
-                <Movie/>
-                <Movie/>
-                <Movie/>
+                {movies.splice(0, 5).map(movie =>
+                    <Movie title={movie.title} rating={movie.vote_average} posterUrl={movie.poster_path} id={movie.id}/>
+                )}
             </div>
         </div>
     )
--- frontend/src/components/Movies/movie/Movie.js
+++ frontend/src/components/Movies/movie/Movie.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Movie.module.css';
-import poster from '../../../assets/images/joker_full.jpg'
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
-function Movie(){
+const API_KEY = process.env.REACT_APP_API_KEY
+
+function Movie(props){
+
+    const [movie, setMovie] = useState([])
+
+    const options = {
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${props.sortBy}&include_adult=false&include_video=false`
+      };
+    
+        const fetchPoster = (id) => {
+            axios.request(options).then(function (response) {
+                setMovie(response.data)
+                console.log(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+
+    useEffect(() => {
+    },[])
+
     return (
         <div className="d-flex flex-column align-items-center">
-            <div className={`card ${styles.movie}`}>
-                <img src={poster} className={`${styles.poster}`} />
-                
+            <Link className={styles.link} to={`/movie/${props.id}`} >
+                <div className={`card ${styles.movie}`}>
+                    
+                    <img src={`https://image.tmdb.org/t/p/original${props.posterUrl}`} className={`${styles.poster}`} />
+                    
+                </div>
+            </Link>
+            <div className={`d-flex flex-column align-items-center mt-1 ${styles.title}`}>
+                <h5>{props.title}</h5>
+                <div className="badge badge-warning">{props.rating}</div>
             </div>
-            <div className="d-flex flex-column align-items-center mt-1">
-                <h4>Title</h4>
-                <div className="badge badge-warning">8.3</div>
-            </div>
-            
         </div>
         
     );
