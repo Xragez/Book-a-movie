d6b613da2d8b390b0b7916c571e5a2d65ab20b95
pawel <pawelzeg@gmail.com>
2021-05-26 16:14:06 +0200

    Change Routing in App.js, add .env to .gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
 import logo from './logo.svg';
-import Login from './pages/Login/Login';
+import Login from './pages/Auth/Login/Login';
 import Home from './pages/Home/Home';
 import './App.css';
 import {
   BrowserRouter as Router, 
   Switch,
   Route,
-  Link
+  Redirect
 } from "react-router-dom";
+import Register from './pages/Auth/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
+          <Redirect exact from="/" to="/home" />
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/home" component={Home}/>
         </Switch>
-         
       </Router>
     </div>
   );
